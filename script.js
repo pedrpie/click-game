c4f437f5d653edeceaa9d195f06b447666bd2db4
pedrpie1 <eupedrohenrique100@gmail.com>
2025-10-15 20:45:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+// botões de tempo
+const button10s = document.querySelector("#button10s"),
+      button20s = document.querySelector("#button20s"),
+      button30s = document.querySelector("#button30s");
+
+// modal
+const modal = document.querySelector("#modal"),
+      clicksModal = document.querySelector("#clicksModal"),
+      fecharModal = document.querySelector("#fecharModal");
+
+// número da contagem da tela
+const countNumber = document.querySelector("#countNumber");
+
+// botão click
+const buttonClick = document.querySelector("#buttonClick");
+
+// segundos
+const seconds = document.querySelector("#seconds");
+
+// contagens
+let count = 0;
+let timer;
+let timerAtivo = false;
+
+let time10s = 10;
+let time20s = 20;
+let time30s = 30;
+
+// eventos
+button10s.addEventListener("click", () => {
+    if (timerAtivo) return;
+    timerAtivo = true;
+    button10s.disabled = true;
+    time10s = 10
+    seconds.textContent = "Tempo: " + time10s + "s"
+
+    timer = setInterval(() => {
+        time10s--;
+        seconds.textContent = "Tempo: " + time10s + "s";
+
+        if (time10s <= 0){
+            clearInterval(timer);
+            button10s.disabled = false;
+            timerAtivo = false;
+            mensagem();
+        }
+        
+    }, 1000)
+});
+
+button20s.addEventListener("click", () => {
+    if (timerAtivo) return;
+    timerAtivo = true;
+    button20s.disabled = true;
+    time20s = 20
+    seconds.textContent = "Tempo: " + time20s + "s"
+
+    timer = setInterval(() => {
+        time20s--;
+        seconds.textContent = "Tempo: " + time20s + "s";
+
+        if (time20s <= 0) {
+            clearInterval(timer)
+            button20s.disabled = false;
+            timerAtivo = false;
+            mensagem()
+        }
+    }, 1000)
+});
+
+button30s.addEventListener("click", () => {
+    if (timerAtivo) return;
+    timerAtivo = true;
+    button30s.disabled = true;
+    time30s = 30
+    seconds.textContent = "Tempo: " + time30s + "s"
+
+    timer = setInterval(() => {
+        time30s--;
+        seconds.textContent = "Tempo: " + time30s + "s";
+    }, 1000)
+
+    if (time30s <= 0) {
+            clearInterval(timer)
+            button30s.disabled = false;
+            timerAtivo = false
+            mensagem();
+        }
+});
+
+buttonClick.addEventListener("click", () => {
+    count++;
+    countNumber.textContent = count
+})
+
+// função e evento do modal
+fecharModal.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
+function mensagem() {
+    modal.style.display = "flex";
+    clicksModal.textContent = "Quantidade de cliques: " + count;
+    count = 0;
+    countNumber.textContent = count;
+}
